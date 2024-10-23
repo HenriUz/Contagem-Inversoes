3142df9dcf3f307ca3f69ffc952afca3f40626f7
HenriUz <d2023001316@unifei.edu.br>
2024-10-23 13:11:53 -0300

    Configs iniciais.

---
+++ main.c
@@ -0,0 +1,16 @@
+/*
+main.c
+
+Descrição: este código tem o objetivo de identificar o número de inversões entre duas listas.
+*/
+
+/* --- Includes. --- */
+
+#include <stdio.h>
+
+/* --- Funções. --- */
+
+int main(void) {
+    printf("Hello, World!\n");
+    return 0;
+}
